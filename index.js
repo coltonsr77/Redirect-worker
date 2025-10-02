36b2ba95c3991c0913ec9c5629376bec2318712f
coltonsr77 <coltonr3@icloud.com>
2025-10-02 17:51:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-export default {
-  async fetch(request) {
-    const destinationURL = "https://coltonsr77.pages.dev";
-    const statusCode = 301;
-    return Response.redirect(destinationURL, statusCode);
-  },
-};
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // The URL you want to redirect to
+  const redirectURL = "https://coltonsr77.pages.dev"; 
+  return Response.redirect(redirectURL, 302) // 301 for permanent, 302 for temporary
+}
