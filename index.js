dc3e6a5ca6c788b6b521b01953d1da0b4d674b9b
coltonsr77 <coltonr3@icloud.com>
2025-10-02 00:44:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     async function handleRequest(request) {
       const url = new URL(request.url)
       if (url.pathname === '/old-path') { // Specify the path to redirect from
-        return Response.redirect('https://www.example.com/new-path', 301) // Specify the target URL and status code
+        return Response.redirect('https://coltonsr77.pages.dev', 301) // Specify the target URL and status code
       }
       return fetch(request)
     }
